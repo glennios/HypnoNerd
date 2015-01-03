110874ef10bcb6eae3710698965fb5d8a741cd44
Glenn Tu <glenn.tu@gmail.com>
2015-01-02 22:59:16 -0800

    Initial Commit

---
+++ HypnoNerd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HypnoNerd
+//
+//  Created by Glenn on 1/2/15.
+//  Copyright (c) 2015 Glenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
