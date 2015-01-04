e53f5a81fac8ddcaff6d20fd003c372fee793659
Glenn Tu <glenn.tu@gmail.com>
2015-01-03 19:26:29 -0800

    set reminder image on tab bar

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -10,6 +10,16 @@
 #import "BNRHypnosisView.h"
 
 @implementation BNRHypnosisViewController
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
+    if (self) {
+        self.tabBarItem.title = @"Reminder";
+        self.tabBarItem.image = [UIImage imageNamed:@"Time.png"];
+    }
+    
+    return self;
+}
 
 // - when a view controller is created (this class here), its self.view is nil, which causes loadView to be called.
 // - init hypnosisView
