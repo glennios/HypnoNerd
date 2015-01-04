c7590933783df9ba7891bc46e977db7c0d7b7c0c
Glenn Tu <glenn.tu@gmail.com>
2015-01-03 17:28:11 -0800

    add BNRReminderViewController

---
+++ HypnoNerd/BNRReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.h
+//  HypnoNerd
+//
+//  Created by Glenn on 1/3/15.
+//  Copyright (c) 2015 Glenn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRReminderViewController : NSObject
+
+@end
---
+++ HypnoNerd/BNRReminderViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.m
+//  HypnoNerd
+//
+//  Created by Glenn on 1/3/15.
+//  Copyright (c) 2015 Glenn. All rights reserved.
+//
+
+#import "BNRReminderViewController.h"
+
+@implementation BNRReminderViewController
+
+@end
